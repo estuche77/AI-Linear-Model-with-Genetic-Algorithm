b972eaa8103f50897871a3ed2c358a69ed1376a1
Jason Latouche <96jaslat@gmail.com>
2018-03-13 20:50:07 -0600

    CIFAR added to the project

--- src/main.py
+++ src/main.py
@@ -3,3 +3,11 @@ Created on Mar 13, 2018
 
 @author: estuche
 '''
+
+import numpy as np
+
+def unpickle(fileName):
+    import pickle
+    with open(fileName, 'rb') as fo:
+        dictionary = pickle.load(fo, encoding='bytes')
+    return dictionary
